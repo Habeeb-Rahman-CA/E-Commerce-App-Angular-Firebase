c7dfb29827c5e6da468876560cbe8423e9298558
HabeebR07 <habeebrahmanca07@gmail.com>
2024-11-18 17:11:50 +0530

    added home section

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,18 @@
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent {
+
+  router = inject(Router)
+
+  goTosignIn(){
+    this.router.navigate(['/sign-in'])
+  }
+}
