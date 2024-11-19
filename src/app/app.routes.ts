3fb1dc1d6b3208a3ff1f5e77d252ffe212e9f29c
HabeebR07 <habeebrahmanca07@gmail.com>
2024-11-19 11:11:36 +0530

    products section clear

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { ProductsComponent } from './components/products/products.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: '', redirectTo: 'home', pathMatch: 'full' },
-    {path:'register', component: RegisterComponent},
-    {path: 'login', component: LoginComponent}
+    { path: 'register', component: RegisterComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'products', component: ProductsComponent }
 
 ];
