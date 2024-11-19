7408c85bf415533ff03dec591c86e8b193fee53d
HabeebR07 <habeebrahmanca07@gmail.com>
2024-11-19 17:00:16 +0530

    added user and admin dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { ProductsComponent } from './components/products/products.component';
 import { AddProductsComponent } from './components/add-products/add-products.component';
+import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
+import { UserDashboardComponent } from './components/user-dashboard/user-dashboard.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
@@ -11,6 +13,8 @@ export const routes: Routes = [
     { path: 'register', component: RegisterComponent },
     { path: 'login', component: LoginComponent },
     { path: 'products', component: ProductsComponent },
-    { path: 'add-products', component: AddProductsComponent }
+    { path: 'add-products', component: AddProductsComponent },
+    { path: 'admin-dashboard', component: AdminDashboardComponent },
+    { path: 'user-dashboard', component: UserDashboardComponent}
 
 ];
