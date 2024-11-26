e1d6312b308914833b4be54f63520f60cf850005
HabeebR07 <habeebrahmanca07@gmail.com>
2024-11-26 10:45:00 +0530

    payment section added

--- src/app/components/payment/payment.component.ts
+++ src/app/components/payment/payment.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-payment',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './payment.component.html',
   styleUrl: './payment.component.css'
 })
 export class PaymentComponent {
+
+  router = inject(Router)
+
+  payment: string = ''
+
+  proceedPayment(){
+   const paymentConfirm = window.confirm(`Are you sure you want to confirm the payment using ${this.payment}?`)
+   if (paymentConfirm) {
+    alert(`Thank you for your payment! You have chosen [${this.payment}] as your payment option. Your transaction is being processed, and you will receive a confirmation email shortly.`)
+    this.router.navigate(['/products'])
+   }
+  }
 
 }
