945c219de8e5d4eb98c4c566159192ea954d1127
HabeebR07 <habeebrahmanca07@gmail.com>
2024-11-27 15:09:54 +0530

    changed url method to dd

--- src/app/components/add-products/add-products.component.ts
+++ src/app/components/add-products/add-products.component.ts
@@ -22,13 +22,38 @@ export class AddProductsComponent {
     productDesc: '',
     productPrice: null,
     productCategory: '',
-    productImgUrl: ''
+    productImgUrl: 'https://www.shutterstock.com/image-vector/upload-document-data-file-cloud-600nw-2297720825.jpg'
   }
-  
-  addProducts(productForm: NgForm){
+
+  addProducts(productForm: NgForm) {
     this.productService.addProducts(this.products)
     productForm.resetForm()
   }
 
+  onDragOver(event: DragEvent) {
+    event.preventDefault()
+  }
 
+  onDrop(event: DragEvent) {
+    event.preventDefault()
+    if (event.dataTransfer?.files) {
+      this.addFile(event.dataTransfer?.files)
+    }
+  }
+
+  addFile(file: FileList) {
+    for (let i = 0; i < file.length; i++) {
+      if (file[i].type.startsWith('image/')) {
+        const imageUrl = URL.createObjectURL(file[i])
+        this.products.productImgUrl = imageUrl
+      }
+    }
+  }
+
+  onInputChange(event: Event) {
+    const input = event.target as HTMLInputElement
+    if (input.files) {
+      this.addFile(input.files)
+    }
+  }
 }
